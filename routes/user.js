57641032f7a981e7897643d96fb986ef8ba948fb
Mgbeahurike Victor <104136490+vhiz@users.noreply.github.com>
2022-07-24 18:38:02 +0100

    security

--- routes/user.js
+++ routes/user.js
@@ -28,7 +28,7 @@ router.put('/:id', verifiedAuth, async (req, res) => {
 router.delete('/:id', verifiedAuth, async (req, res) => {
     const user = await User.findById(req.params.id)
     const deleteuser = await bcrypt.compare(req.body.password, user.password)
-    if(!deleteuser)return res.status(400).send('Input correct password before deleting your account')
+    if(!deleteuser)return res.status(400).send('Input correct password before deleting your account ')
     try {
         await User.findByIdAndDelete(req.params.id)
         res.status(200).send('User has been deleted')
